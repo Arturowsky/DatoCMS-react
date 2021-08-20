c62716eef0a9150614d335f5ea35a16c46b82f88
Artur <artur.lewandowicz@gmail.com>
2021-08-20 03:14:10 +0200

    lets see

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ContextProvider } from "./Context"
 
@@ -6,15 +6,16 @@ import Header from "./Header";
 import Home from "./Home";
 import About from "./About";
 import Recipe from "./Recipe";
-import Artur from "./Artur";
+import Homepage from "./Homepage";
 // import { useContext } from "react";
 
 const App = () => (
   <ContextProvider>
     <Router>
+    
       <Header />
       <main>
-        <Route exact path="/" component={Artur} />
+        <Route exact path="/" component={Homepage} />
         <Route path="/blog" component={Home} />
         <Route path="/recipes/:slug" component={Recipe} />
         <Route path="/about" component={About} />
